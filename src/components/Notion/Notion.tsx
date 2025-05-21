69cdd82db6c73b0474fdb5d9913cc8e0eb341389
yuri <jyuri1114@naver.com>
2025-05-21 18:18:01 +0900

    [add] 링크 첨부 완

--- src/components/Notion/Notion.tsx
+++ src/components/Notion/Notion.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import styles from "./Notion.module.scss";
 
 const Notion = () => {
-  return <button className={styles.notionBtn}>자기소개 보러가기</button>;
+  const openNotion = () => {
+    window.open(
+      "https://three-hamster-1bb.notion.site/JANG-YURI-334564ac85b34aab991762332c21eeb4?pvs=4",
+      "_blank"
+    );
+  };
+
+  return (
+    <button className={styles.notionBtn} onClick={openNotion}>
+      자기소개 보러가기
+    </button>
+  );
 };
 
 export default Notion;
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,14 +6,15 @@ export const projects: Project[] = [
     title: "yuri-portfolio",
     description: "장유리 포트폴리오 사이트",
     tags: ["React", "TypeScript", "scss"],
-    image: "/thumbnail04.png",
-    notionLink: "https://www.notion.so/...",
+    image: `${import.meta.env.BASE_URL}/thumbnail04.png`,
+    notionLink:
+      "https://three-hamster-1bb.notion.site/yuri-portfolio-1e487d8f0b528035883af623e8f6a8bb?pvs=4",
   },
   {
     title: "moodie 무디",
     description: "TMDB API를 기반으로 제작한 영화 정보 탐색 웹앱",
     tags: ["React", "JavaScript", "scss"],
-    image: "/thumbnail01.png",
+    image: `${import.meta.env.BASE_URL}/thumbnail01.png`,
     notionLink:
       "https://three-hamster-1bb.notion.site/movie-web-moodie-1e487d8f0b52800cb8e6ee2bc64b2771?pvs=4",
   },
@@ -21,7 +22,7 @@ export const projects: Project[] = [
     title: "mee-gam 미감",
     description: "React 쇼핑몰 SPA 구현 프로젝트",
     tags: ["React", "JavaScript", "scss"],
-    image: "/thumbnail02.png",
+    image: `${import.meta.env.BASE_URL}/thumbnail02.png`,
     notionLink:
       "https://three-hamster-1bb.notion.site/mee-gam-1e487d8f0b528055b19dfe966800a657?pvs=4",
   },
@@ -29,7 +30,7 @@ export const projects: Project[] = [
     title: "weather-web",
     description: "OpenWeather API를 활용해 현재 위치와 특정 도시들의 날씨 표시",
     tags: ["React", "JavaScript", "css"],
-    image: "/thumbnail03.png",
+    image: `${import.meta.env.BASE_URL}/thumbnail03.png`,
     notionLink:
       "https://three-hamster-1bb.notion.site/weather-web-1e487d8f0b5280a8b89aecf2a1990bd2?pvs=4",
   },
@@ -37,7 +38,7 @@ export const projects: Project[] = [
     title: "TO DO LIST",
     description: "자바스크립트를 공부하며 만들어본 투두 프로젝트",
     tags: ["JavaScript", "css"],
-    image: "/thumbnail05.png",
+    image: `${import.meta.env.BASE_URL}/thumbnail05.png`,
     notionLink:
       "https://three-hamster-1bb.notion.site/TO-DO-LIST-1e487d8f0b52803691bfe2aad265055c?pvs=4",
   },
