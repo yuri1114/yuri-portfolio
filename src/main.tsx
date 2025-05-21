a8a3447cc265bdc197ef7f426a4768ddf3d2d16a
yuri <jyuri1114@naver.com>
2025-05-21 14:28:51 +0900

    [add] 배포

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}>
     <App />
   </BrowserRouter>
 );
